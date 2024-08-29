4f445f5ecb33c0bbd68c5c894b6f529406c74696
karina-m1205 <karina.movsesyan.95@mail.ru>
2024-08-29 17:46:13 +0400

    second_commit

--- src/core/db.js
+++ src/core/db.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const URI = process.env.mongodbURI || "mongodb://localhost:27017/social_network_backend";
 
+
+
 const db = mongoose.connect(URI);
 
 module.exports = db;
